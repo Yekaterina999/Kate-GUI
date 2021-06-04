1bd18ee5306a85f59e462cb7c8d8e5922e150b48
Yekaterina999 <yekaterina999@outlook.com>
2021-06-04 23:43:50 +0100

    update

--- gui/common/mod~KateGUI.js
+++ gui/common/mod~KateGUI.js
@@ -1,8 +1,3 @@
-KateGUI_patchApplyN("hasSameMods", function (target, that, args)
-{
-	let mod = ([name, version]) => !/^FGod.*/i.test(name);
-	return target.apply(that, args.map(mods => mods.filter(mod)));
-})
 
 KateGUI_patchApplyN("hasSameMods", function (target, that, args)
 {
